37081f1166b303a0961f1d91134a3b9ff96aebab
Diego Leandro <d.deleon17@unapec.edu.do>
2023-07-07 19:13:12 -0400

    style:
    I've added styling to the navbar section of this GPT-3 practice page.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+// import {Rimenu3Line, RicloseLine} from "react-icons/ri";
 import './navbar.css';
+import logo from "../../assets/logo.svg";
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='gpt3__navbar'>
+      <div className='gpt3___navbar-links'>
+        <div className='gpt3___navbar-links_logo'>
+          <img  src={logo} alt='logo'/>
+        </div>
+        <div className='gpt3___navbar-links_containers'>
+          <p><a href='#home'>Home</a></p>
+          <p><a href='#wgpt3'>What is GPT-3?</a></p>
+          <p><a href='#possibility'>Open AI</a></p>
+          <p><a href='#features'>Case Studies</a></p>
+          <p><a href='#blog'>Library</a></p>
+        </div>
+      </div>
+      <div className='gpt3___navbar-sign '>
+          <p>Sign in</p>
+          <button type='button'>Sign Up</button>
+      </div>
+    </nav>
   )
 }
 
